f36257fb1351aacc30b3b11835c5ca1abd97ac3f
dkaiser21 <f.guajardo734@gmail.com>
2021-08-14 16:12:24 -0500

    0.00000002

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>Twitter.It´s what happening</title>
+    </head>
+    <body>
+        <section class="main-page">
+        <div class="left">left</div>
+        <div class="right">Right</div>
+        <footer aria-label="footer" class="main-page-footer"></footer>
+        </section>
+    </body>
+</html>
