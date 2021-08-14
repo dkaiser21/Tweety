b7bc91ac61212fcbce78b9fb7fd804937738fd19
dkaiser21 <f.guajardo734@gmail.com>
2021-08-14 16:19:47 -0500

    movimiento de CSS

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width-device-width, initial-scale=1.0">
         <title>Twitter.It´s what happening</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <section class="main-page">
-        <div class="left">left</div>
+        <div class="left">Left</div>
         <div class="right">Right</div>
-        <footer aria-label="footer" class="main-page-footer"></footer>
+        <footer aria-label="Footer" class="main-page-footer">Footer</footer>
         </section>
     </body>
 </html>
