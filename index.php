e9789d76c4dcee31597f022f95f922d40763a91b
Cahedan <f.guajardo734@gmail.com>
2021-08-19 11:23:37 -0500

    Termino de Footer

--- index.php
+++ index.php
@@ -18,19 +18,39 @@
         <div class="right">
             <div class="right-content">
                 <div class="right-site-wrapper">
-                <svg xmlns="http://www.w3.org/2000/svg" style="width:45px;height:57px;" viewBox="0 0 24 24" ><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"/></g></svg>
-                </div>
-                <div class="right-middle-content">
-                <h2 class="middle-heading">happening Now</h2>
-                <h5>Join Tweety today</h5>
-                    <div class="s-in-wrapper">
-                    <a href="sign.php" class="btn-link sign-up-btn">Sign Up</a>
-                    <a href="login.php" class="btn-link log-in-btn">Log In</a>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="width:45px;height:57px;" viewBox="0 0 24 24" ><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"/></g></svg>
                     </div>
-                </div>
+                <div class="right-middle-content">
+                    <h2 class="middle-heading">happening Now</h2>
+                    <h5>Join Tweety today</h5>
+                    <div class="s-in-wrapper">
+                        <a href="sign.php" class="btn-link sign-up-btn">Sign Up</a>
+                        <a href="login.php" class="btn-link log-in-btn">Log In</a>
+                        </div>
+                    </div>
             </div>
         </div>
-        <footer aria-label="Footer" class="main-page-footer">Footer</footer>
+        <footer aria-label="Footer" class="main-page-footer">
+            <nav role="navigation">
+            <a href="#">About</a>
+            <a href="#">Help Center</a>
+            <a href="#">Terms of Service</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Ads Info</a>
+            <a href="#">Blog</a>
+            <a href="#">Status</a>
+            <a href="#">Careers</a>
+            <a href="#">Brand</a>
+            <a href="#">Advertising</a>
+            <a href="#">Marketing</a>
+            <a href="#">Twitter for Business</a>
+            <a href="#">Developers</a>
+            <a href="#">Directory</a>
+            <a href="#">Settings</a>
+            <span href="#">&copy; <?php echo date('y'); ?> Tweety, inc </span>
+            
+            </nav>
+        </footer>
         </section>
     </body>
 </html>
