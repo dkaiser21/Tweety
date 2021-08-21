d18c87979f462bb8d0c8b3f53663d31d9e3031b7
Cahedan <f.guajardo734@gmail.com>
2021-08-21 10:22:53 -0500

    Creacion del Login

---
+++ backend/shared/signNav.php
@@ -0,0 +1,16 @@
+<nav class="nav-header-sign__up">
+        <ul>
+            <li>
+                <a href="index.php">
+                    <i class="fa fa-twitter"></i>
+                        home
+                </a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Language:English</a>
+            </li>
+        </ul>
+    </nav>
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php $pageTitle="Login on Tweety | Tweety"; ?>
+<?php require_once 'backend/shared/header.php'; ?>
+<section class="sign-container">
+    <?php require_once 'backend/shared/signNav.php'; ?>
+    <div class="form-container">
+        <div class="form-content">
+            <div class="header-form-content">
+                <h2>Login to Tweety<h2>
+            </div>
+            <form action="sign.php" class="formField">
+                <div class="form-group">
+                    <label for="un">Username or Email</label>
+                    <input type="text" name="un" id="un" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <label for="pass">Password</label>
+                    <input type="password" name="pass" id="pass" autocomplete="off">
+                </div>
+                <div class="spassword">
+                    <input type="checkbox" class="form-checkbox" id="s-password" >
+                    <label for="s-password">Show Password</label>
+                </div>
+                <div class="form-btn-wrapper">
+                    <button type="submit" class="btn-form">SignUp</button>
+                    <input type="checkbox" class="form-checkbox" id="check" name="remember">
+                    <label for="check">Remember me</label>
+                </div>
+        </div>
+        <footer class="form-footer">
+        <p>New to Tweety? <a href="sign.php">signup for tweety</a></p>
+    </footer>
+    </div>
+    
+</section>  
