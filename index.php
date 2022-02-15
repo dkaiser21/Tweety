34aa149a5807d6e443d520b9cbf133ff353e8c71
dkaiser21 <f.guajardo734@gmail.com>
2022-02-15 18:54:15 -0300

    actualizacion

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
    $objeto = new Conexion();
    $conexion = $objeto->Conectar();
 
-   $consulta = "SELECT * FROM grilla";
+   $consulta = "SELECT * FROM modulos";
    $resultado = $conexion->prepare($consulta);
    $resultado -> execute();
    $datos=$resultado->fetchALL(PDO::FETCH_ASSOC);
@@ -20,7 +20,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>tabla de procesos</title>
+    <title>Modulos</title>
     <style>
         table.dataTable thead {
             background: black;
@@ -29,19 +29,15 @@
     </style> 
   </head>
   <body>
-  <div class="container">
-       <div class="row">
-           <div class="col-lg-12">
-              <table id="Grillas" class="table table-striped" style="width:100%">
+  <a href="http://localhost/crontab/">Ir a Crontab</a>
+              <table id="Grillas" class="table table-striped" style="width:100%" >
                 <thead class="text-center">
                   <tr>
-                    <th>ID</th>
-                    <th>PROCESO</th>
-                    <th>DESCRIPCIÓN</th>
-                    <th>HOST_PROCESO</th>
-                    <th>PATH_PROCESO</th>
-                    <th>HOST_TIPO</th>
-                    <th>PROCESO_TIPO</th>
+                    <th>ID_MODULO</th>
+                    <th>DESC_MODULO</th>
+                    <th>PADRE</th>
+                    <th>LEVEL</th>
+                    <th>UBICACION</th>
                   </tr>
                 </thead>
         <tbody>
@@ -49,22 +45,17 @@
 foreach($datos as $dato){
 ?>
 <tr>
-            <th><?php echo $dato['ID']?></th>
-            <th><?php echo $dato['PROCESO']?></th>
-            <th><?php echo $dato['DESCRIPCION']?></th>
-            <th><?php echo $dato['HOST_PROCESO']?></th>
-            <th><?php echo $dato['PATH_PROCESO']?></th>
-            <th><?php echo $dato['HOST_TIPO']?></th>
-            <th><?php echo $dato['PROCESO_TIPO']?></th>
+            <th><?php echo $dato['ID_MODULO']?></th>
+            <th><?php echo $dato['DESC_MODULO']?></th>
+            <th><?php echo $dato['PADRE']?></th>
+            <th><?php echo $dato['LEVEL']?></th>
+            <th><?php echo $dato['UBICACION']?></th>
           </tr>
 <?php
 }
 ?>
         </tbody>
     </table>
-        </div>
-       </div> 
-    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
