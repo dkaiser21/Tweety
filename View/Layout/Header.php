656b957eb320159d0a502700049c5bce37e3012c
dkaiser21 <f.guajardo734@gmail.com>
2022-02-17 10:58:45 -0300

    actualizacion de links

--- View/Layout/Header.php
+++ View/Layout/Header.php
@@ -12,4 +12,25 @@
             color:white;
         }
     </style> 
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand">Menu de Navegacion</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="..\Pages\Crontab.php">CRONTAB</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="..\Pages\Modulos.php">MODULOS</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
   </head>
--- index.php
+++ index.php
@@ -7,11 +7,29 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Menu</title>
   </head>
   <body>
-  <a href="./View/pages/Modulos.php">Ir a Modulos</a><br><br>
-  <a href="./View/pages/Crontab.php">Ir a Crontab</a>
+
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Menu de Navegacion</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./View/pages/Crontab.php">CRONTAB</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./View/pages/Modulos.php">MODULOS</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
